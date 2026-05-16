41e6a263f59d8c75f57199d1ba6278da1722537c
Lugo Salazar Brandon Oziel <221G0304@rcarbonifera.tecnm.mx>
2026-05-15 18:19:54 -0600

    Agregar archivos de proyecto.

---
+++ PoryectoClienteServidor/Models/DTOs/RegistroDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoryectoClienteServidor.Models.DTOs
+{
+    internal class RegistroDTO
+    {
+        public string Nombre { get; set; } = null!;
+        public int Turno { get; set; } = 0;
+    }
+}
---
+++ PoryectoClienteServidor/Models/DTOs/Tablero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoryectoClienteServidor.Models.DTOs
+{
+    public class Tablero
+    {
+        public string TurnoQuien { get; set; } = null!;
+        public string EstadoTurno { get; set; } = null!;
+    }
+}
