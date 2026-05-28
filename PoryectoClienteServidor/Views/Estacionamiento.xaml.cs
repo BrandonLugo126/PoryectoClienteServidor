809065f62d8e26747b59ca5e5d0c2d143ea7ec7d
HP <HP@DESKTOP-VV67OVT>
2026-05-28 17:45:21 -0600

    .

---
+++ PoryectoClienteServidor/Views/Estacionamiento.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PoryectoClienteServidor.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Estacionamiento.xaml
+    /// </summary>
+    public partial class Estacionamiento : UserControl
+    {
+        public Estacionamiento()
+        {
+            InitializeComponent();
+        }
+    }
+}
